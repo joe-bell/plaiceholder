c7de8cfb06a2e7afa43cfe6f3b1356d863b4e475
joe-bell <joe@joebell.co.uk>
2021-06-03 09:33:08 +0300

    chore: extend format and checks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+  "*.json": (filenames) => {
+    const hasPackageJson = filenames.some((filename) =>
+      filename.includes("package.json")
+    );
+
+    return hasPackageJson ? ["yarn syncpack format"] : [];
+  },
   "*.{ts,tsx,jsx,jsx,json,html,css,md,mdx,yml}": (filenames) =>
-    filenames.map((filename) => `yarn format --write '${filename}'`),
-  "*.ts?(x)": () => ["yarn lint"],
+    filenames.map((filename) => `yarn prettier --write '${filename}'`),
+  "*.ts?(x)": () => ["yarn lint:ts"],
 };
