1421547a97e2572e533a2d8b88f2cb7bc1ca90a0
joe-bell <joe@joebell.co.uk>
2021-06-10 11:06:20 +0300

    chore(example-with-11ty): add instant.page

--- examples/11ty/src/_layouts/root.11ty.js
+++ examples/11ty/src/_layouts/root.11ty.js
@@ -115,6 +115,8 @@ exports.render = function (data) {
     <script>
       document.body.classList.remove('preload');
     </script>
+    <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw">
+    </script>
   </body>
 </html>`;
 };
