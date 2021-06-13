a88a79b82078cce87cd761775cd95f84685b7ad5
joe-bell <joe@joebell.co.uk>
2021-06-13 14:09:20 +0300

    chore(example-with-next): add sharp to externals

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -4,4 +4,14 @@ module.exports = withPlaiceholder({
   images: {
     domains: ["images.unsplash.com"],
   },
+  webpack: (config) => {
+    config.externals.push({
+      sharp: "commonjs sharp",
+    });
+
+    return config;
+  },
+  future: {
+    webpack5: true,
+  },
 });
