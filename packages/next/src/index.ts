76a49befa385133ecb7171409c347561088bd0e8
joe-bell <joe@joebell.co.uk>
2021-06-13 14:27:10 +0300

    fix(@plaiceholder/next): add sharp to webpack externals

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,5 +1,17 @@
-export const withPlaiceholder = (config) => {
+export const withPlaiceholder = (nextConfig) => {
   require("sharp");
 
-  return config;
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      config.externals.push({
+        sharp: "commonjs sharp",
+      });
+
+      if (typeof nextConfig?.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+
+      return config;
+    },
+  });
 };
