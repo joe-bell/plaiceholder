d8604f533178ecf480e4dbcf534de8a23180b5c6
joe-bell <joe-bell@users.noreply.github.com>
2022-08-26 19:06:34 +0700

    chore(example-with-remix): use relative dir for unsplash images

--- examples/remix/app/lib/images.server.ts
+++ examples/remix/app/lib/images.server.ts
@@ -2,13 +2,18 @@ import path from "path";
 import glob from "glob";
 
 export const getAllUnsplashImagePaths = (): string[] =>
-  glob.sync("./public/assets/images/unsplash/*.{jpg,png}").map((file) => {
-    const sep = "/";
-    const fileArr = file.split(sep);
+  glob
+    .sync(
+      // See https://github.com/remix-run/remix/discussions/4074
+      path.join(__dirname, "../public/assets/images/unsplash/*.{jpg,png}")
+    )
+    .map((file) => {
+      const sep = "/";
+      const fileArr = file.split(sep);
 
-    const filePath = fileArr
-      .slice(fileArr.indexOf("public") + 1, fileArr.length)
-      .join(sep);
+      const filePath = fileArr
+        .slice(fileArr.indexOf("public") + 1, fileArr.length)
+        .join(sep);
 
-    return [sep, filePath].join("");
-  });
+      return [sep, filePath].join("");
+    });
