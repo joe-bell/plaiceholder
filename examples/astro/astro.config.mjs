f52fec00fff4554e4758d5020877fc84568b0760
joe-bell <joe-bell@users.noreply.github.com>
2022-08-31 16:33:33 +0700

    chore(example-with-astro): use astro image component

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [image(), tailwind()],
-  ssr: {
-    external: ["@astrojs/image", "sharp", "tiny-glob"],
-  },
 });
