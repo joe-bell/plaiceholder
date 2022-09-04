b4cade13ec66e6038f814d5a1bc34dbdac458387
joe-bell <joe-bell@users.noreply.github.com>
2022-09-04 09:53:46 +0700

    chore(example-with-astro): use regular img for multiple, self-host instant.page

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -5,4 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [image(), tailwind()],
+  vite: {
+    ssr: {
+      external: ["image-size", "tiny-glob"],
+    },
+  },
 });
