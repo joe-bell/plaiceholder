a374fb3052f705371afc8229b9488af34ddc2bec
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-05-11 09:04:05 +0300

    chore(@plaiceholder/tailwindcss): manual fs

--- packages/tailwindcss/src/plugin.ts
+++ packages/tailwindcss/src/plugin.ts
@@ -1,12 +1,17 @@
 import plugin from "tailwindcss/plugin";
+
 import makeSynchronous from "make-synchronous";
 import type { IGetPlaiceholder } from "plaiceholder";
 import { classNamePrefix } from "./config";
 
 const getPlaiceholder = makeSynchronous(async (imageUrl) => {
+  const fs = require("node:fs/promises");
+  const path = require("node:path");
   const { getPlaiceholder } = require("plaiceholder");
 
-  const { css } = await (getPlaiceholder as IGetPlaiceholder)(imageUrl);
+  const buffer = await fs.readFile(path.join("./public", imageUrl));
+
+  const { css } = await (getPlaiceholder as IGetPlaiceholder)(buffer);
 
   return css;
 });
