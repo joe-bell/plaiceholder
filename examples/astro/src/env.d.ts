7f0eeec6ae53a78ef19c0034ce0401af7df7f674
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-05-18 16:27:12 +0300

    chore(example-with-astro): update to astro@2.0, use assets

--- examples/astro/src/env.d.ts
+++ examples/astro/src/env.d.ts
@@ -1,2 +1,1 @@
-// Replace `astro/client` with `@astrojs/image/client`
-/// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client-image" />
--- examples/next/tailwind.config.js
+++ examples/next/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./node_modules/@plaiceholder/ui/**/*.{ts,tsx}",
