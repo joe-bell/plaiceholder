4868ce77dc0f6fd6360746f89b7f9e168c8b17a8
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-05-22 13:15:55 +0300

    refactor(plaiceholder): make esm only

--- packages/tailwindcss/src/plugin.ts
+++ packages/tailwindcss/src/plugin.ts
@@ -3,7 +3,8 @@ import makeSynchronous from "make-synchronous";
 import { classNamePrefix } from "./config.js";
 
 const getPlaiceholder = makeSynchronous(async (buffer) => {
-  const { css } = await require("plaiceholder").getPlaiceholder(buffer);
+  const { getPlaiceholder } = await import("plaiceholder");
+  const { css } = await getPlaiceholder(buffer);
 
   return css;
 });
